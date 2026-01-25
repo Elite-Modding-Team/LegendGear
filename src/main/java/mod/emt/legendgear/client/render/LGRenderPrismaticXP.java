2f453ab5a101380089b25ae9f2484c92aa7abae3
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-01-24 19:44:19 -0600

    Port XP render improvements from LG2

--- src/main/java/mod/emt/legendgear/client/render/LGRenderPrismaticXP.java
+++ src/main/java/mod/emt/legendgear/client/render/LGRenderPrismaticXP.java
@@ -14,6 +14,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+// TODO: Position could be adjusted so it doesn't sink too much into the ground
 @SideOnly(Side.CLIENT)
 public class LGRenderPrismaticXP extends Render<EntityXPOrb>
 {
@@ -22,6 +23,8 @@ public class LGRenderPrismaticXP extends Render<EntityXPOrb>
     public LGRenderPrismaticXP(RenderManager renderManager)
     {
         super(renderManager);
+        this.shadowSize = 0.15F;
+        this.shadowOpaque = 0.75F;
     }
 
     protected ResourceLocation getEntityTexture(EntityXPOrb entity)
@@ -29,40 +32,72 @@ public class LGRenderPrismaticXP extends Render<EntityXPOrb>
         return prismaticLegendOrbs;
     }
 
-    public void renderTheXPOrb(EntityXPOrb orb, double x, double y, double z, float entityYaw, float partialTicks)
-    {
+    public void renderTheXPOrb(EntityXPOrb orb, double x, double y, double z, float entityYaw, float partialTicks) {
         GlStateManager.pushMatrix();
         GlStateManager.translate((float) x, (float) y, (float) z);
         int frame = orb.getTextureByXP();
-        bindTexture(prismaticLegendOrbs);
+        this.bindEntityTexture(orb);
         Tessellator tess = Tessellator.getInstance();
         BufferBuilder buffer = tess.getBuffer();
-        float uMin = (frame % 4 * 16) / 64.0F;
-        float uMax = (frame % 4 * 16 + 16) / 64.0F;
-        float vMin = (frame / 4 * 16) / 64.0F;
-        float vMax = (frame / 4 * 16 + 16) / 64.0F;
-        float size = 0.3F;
+        float uMin = (float) (frame % 4 * 16 + 0) / 64.0F;
+        float uMax = (float) (frame % 4 * 16 + 16) / 64.0F;
+        float vMin = (float) (frame / 4 * 16 + 0) / 64.0F;
+        float vMax = (float) (frame / 4 * 16 + 16) / 64.0F;
+        float var16 = 1.0F;
+        float var17 = 0.5F;
+        float var18 = 0.25F;
         int light = 240;
         int lightU = light % 65536;
         int lightV = light / 65536;
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightU, lightV);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) lightU / 1.0F, (float) lightV / 1.0F);
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        float phase = (float) ((Minecraft.getSystemTime() % 1000L) / 1000.0D + (orb.posX + orb.posZ) * 0.05D);
+        float var26 = 255.0F;
+
+        float time = (Minecraft.getSystemTime() % 1000000) / 50f;
+        float phase = (float) ((Minecraft.getSystemTime() % 1000 / 1000f) + (orb.posX + orb.posZ) * 0.05);
         float r = Rainbow.r(phase);
         float g = Rainbow.g(phase);
         float b = Rainbow.b(phase);
-        int rgba = (int) (r * 255.0F) << 16 | (int) (g * 255.0F) << 8 | (int) (b * 255.0F);
+
+        lightV = (int) (r * var26);
+        int var22 = (int) (g * var26);
+        int var23 = (int) (b * var26);
+        int var24 = lightV << 16 | var22 << 8 | var23;
         GlStateManager.rotate(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(((this.renderManager.options.thirdPersonView == 2) ? -1 : 1) * -this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-        GlStateManager.scale(size, size, size);
+        GlStateManager.rotate(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+        float var25 = 0.3F;
+        GlStateManager.scale(var25, var25, var25);
         GlStateManager.disableLighting();
-        buffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
-        buffer.pos(-0.5D, -0.25D, 0.0D).tex(uMin, vMax).color(r, g, b, 1.0F).endVertex();
-        buffer.pos(0.5D, -0.25D, 0.0D).tex(uMax, vMax).color(r, g, b, 1.0F).endVertex();
-        buffer.pos(0.5D, 0.75D, 0.0D).tex(uMax, vMin).color(r, g, b, 1.0F).endVertex();
-        buffer.pos(-0.5D, 0.75D, 0.0D).tex(uMin, vMin).color(r, g, b, 1.0F).endVertex();
+        buffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+
+        buffer.pos((double) (0.0F - var17), (double) (0.0F - var18), 0.0D)
+                .tex((double) uMin, (double) vMax)
+                .color(r, g, b, 1.0F)
+                .normal(0.0F, 1.0F, 0.0F)
+                .endVertex();
+
+        buffer.pos((double) (var16 - var17), (double) (0.0F - var18), 0.0D)
+                .tex((double) uMax, (double) vMax)
+                .color(r, g, b, 1.0F)
+                .normal(0.0F, 1.0F, 0.0F)
+                .endVertex();
+
+        buffer.pos((double) (var16 - var17), (double) (1.0F - var18), 0.0D)
+                .tex((double) uMax, (double) vMin)
+                .color(r, g, b, 1.0F)
+                .normal(0.0F, 1.0F, 0.0F)
+                .endVertex();
+
+        buffer.pos((double) (0.0F - var17), (double) (1.0F - var18), 0.0D)
+                .tex((double) uMin, (double) vMin)
+                .color(r, g, b, 1.0F)
+                .normal(0.0F, 1.0F, 0.0F)
+                .endVertex();
+
         tess.draw();
-        GlStateManager.enableLighting();
+        GlStateManager.disableLighting();
+        GlStateManager.disableBlend();
+        GlStateManager.disableRescaleNormal();
         GlStateManager.popMatrix();
     }
 
