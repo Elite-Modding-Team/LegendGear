377e861256a4852bd1b150cef0a14fa581af1ded
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-01-31 20:17:34 +0100

    Prepare packet handler

--- src/main/java/mod/emt/legendgear/network/LGPacketHandler.java
+++ src/main/java/mod/emt/legendgear/network/LGPacketHandler.java
@@ -1,21 +1,28 @@
 package mod.emt.legendgear.network;
 
-import mod.emt.legendgear.LegendGear;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
+
+import mod.emt.legendgear.LegendGear;
 
 public class LGPacketHandler
 {
-    public static SimpleNetworkWrapper INSTANCE;
+    public static SimpleNetworkWrapper instance;
 
     public static void init()
     {
         int id = 0;
-    }
+        instance = NetworkRegistry.INSTANCE.newSimpleChannel(LegendGear.MOD_ID);
 
-    static
-    {
-        INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(LegendGear.MOD_ID);
+        // Client packets
+        if (FMLLaunchHandler.side().isClient())
+        {
+        }
+        // Server packets
+        else
+        {
+
+        }
     }
 }
