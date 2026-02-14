2eda4ec4c443a707df346f308f36ea54912bcb62
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-02-14 14:30:36 +0100

    Overhaul fortune cookies, add hints

--- src/main/java/mod/emt/legendgear/item/LGItemFortuneCookie.java
+++ src/main/java/mod/emt/legendgear/item/LGItemFortuneCookie.java
@@ -5,14 +5,15 @@ import javax.annotation.Nullable;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import java.util.List;
-import mod.emt.legendgear.LegendGear;
 import mod.emt.legendgear.init.LGSoundEvents;
 import mod.emt.legendgear.item.base.LGItemFood;
 import mod.emt.legendgear.util.TooltipHelper;
@@ -36,23 +37,27 @@ public class LGItemFortuneCookie extends LGItemFood
     {
         if (!world.isRemote)
         {
-            // TODO: Custom fortunes via a config list?
             // 1/3 chance to give a useful tip, otherwise give us a generic message
-            /*if (world.rand.nextInt(2) == 0)
+            if (world.rand.nextInt(3) == 0)
             {
-                player.sendMessage(new TextComponentTranslation("fortunes." + LegendGear.MOD_ID + ".hint." + world.rand.nextInt(23)).setStyle(new Style().setItalic(true).setColor(TextFormatting.GOLD)));
-            } else
+                player.sendMessage(new TextComponentTranslation("fortunes.legendgear.hint." + world.rand.nextInt(countMessages("fortunes.legendgear.hint"))).setStyle(new Style().setItalic(true).setColor(TextFormatting.GOLD)));
+                player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 0.8F, 1.2F);
+                // TODO: Advancement
+            }
+            else
             {
-                player.sendMessage(new TextComponentTranslation("fortunes." + LegendGear.MOD_ID + ".generic." + world.rand.nextInt(51)).setStyle(new Style().setItalic(true)));
-            }*/
-
-            player.sendMessage(new TextComponentTranslation("fortunes." + LegendGear.MOD_ID + ".generic." + world.rand.nextInt(51)).setStyle(new Style().setItalic(true)));
-
-            // TODO: Advancement?
-            //player.addStat((StatBase)LegendGear2.achievementFortunate, 1);
+                player.sendMessage(new TextComponentTranslation("fortunes.legendgear.generic." + world.rand.nextInt(countMessages("fortunes.legendgear.generic"))).setStyle(new Style().setItalic(true)));
+                player.world.playSound(null, player.posX, player.posY, player.posZ, LGSoundEvents.ITEM_FORTUNE_COOKIE_USE.getSoundEvent(), SoundCategory.PLAYERS, 2.0F, 0.8F + world.rand.nextFloat() * 0.4F);
+            }
         }
 
-        player.playSound(LGSoundEvents.ITEM_FORTUNE_COOKIE_USE.getSoundEvent(), 2.0F, 0.8F + world.rand.nextFloat() * 0.4F);
         super.onFoodEaten(stack, world, player);
+    }
+
+    private int countMessages(String translateKey)
+    {
+        int i;
+        for (i = 0; !I18n.format(translateKey + "." + i).contains(translateKey); i++) ;
+        return i;
     }
 }
