e3a54eccce2a26100558bdf94dd8027f07edb8cf
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-02-14 19:14:39 +0100

    Port dimensional catalyst

--- src/main/java/mod/emt/legendgear/item/LGItemDimensionalCatalyst.java
+++ src/main/java/mod/emt/legendgear/item/LGItemDimensionalCatalyst.java
@@ -1,52 +1,86 @@
 package mod.emt.legendgear.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import mod.emt.legendgear.init.LGBlocks;
+import mod.emt.legendgear.init.LGItems;
 
 public class LGItemDimensionalCatalyst extends Item
 {
+
+    public LGItemDimensionalCatalyst()
+    {
+        super();
+        setMaxStackSize(16);
+    }
+
     @Override
     public EnumRarity getRarity(ItemStack stack)
     {
         return EnumRarity.UNCOMMON;
     }
 
-    // TODO: Port functionality
-    /*public boolean onItemUseFirst(final ItemStack stack, final EntityPlayer player, final World world, final int x, final int y, final int z, final int side, final float hitX, final float hitY, final float hitZ) {
-        final Block block = world.getBlock(x, y, z);
-        final int meta = world.getBlockMetadata(x, y, z);
-        if (player.capabilities.allowEdit && (block.canHarvestBlock(player, meta) || block == LegendGear2.azuriteOreBlock) && world.canMineBlock(player, x, y, z) && !block.hasTileEntity(meta) && block.getMobilityFlag() == 0 && !world.isRemote) {
-            if (block == LegendGear2.azuriteOreBlock) {
-                final int dx = EnumFacing.values()[side].getFrontOffsetX();
-                final int dy = EnumFacing.values()[side].getFrontOffsetY();
-                final int dz = EnumFacing.values()[side].getFrontOffsetZ();
-                world.setBlock(x, y, z, Blocks.stone, 0, 3);
-                for (int j = 0; j < 3; ++j) {
-                    final EntityItem item = new EntityItem(world, x + 0.5 + dx, y + 0.5 + dy, z + 0.5 + dz, new ItemStack((Item)LegendGear2.azurite, 1, 1));
-                    world.spawnEntityInWorld((Entity)item);
-                }
-                world.playSoundEffect((double)x, (double)y, (double)z, "mob.endermen.portal", 1.0f, 1.0f);
-                --stack.stackSize;
-                player.addStat((StatBase)LegendGear2.achievementAzurite, 1);
-                return true;
+    @Override
+    public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand)
+    {
+        if (world.isRemote || !player.capabilities.allowEdit || !world.isBlockModifiable(player, pos))
+        {
+            return EnumActionResult.PASS;
+        }
+
+        ItemStack stack = player.getHeldItem(hand);
+        IBlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
+
+        // Special behavior: Replace targeted azurite ore with stone, drop azurite
+        if (block == LGBlocks.AZURITE_ORE)
+        {
+            int dx = side.getXOffset();
+            int dy = side.getYOffset();
+            int dz = side.getZOffset();
+            world.destroyBlock(pos, false);
+            world.setBlockState(pos, Blocks.STONE.getDefaultState());
+            for (int j = 0; j < 3; j++)
+            {
+                EntityItem itemEntity = new EntityItem(world, pos.getX() + 0.5 + dx, pos.getY() + 0.5 + dy, pos.getZ() + 0.5 + dz, new ItemStack(LGItems.AZURITE, 1));
+                world.spawnEntity(itemEntity);
             }
-            for (int i = 0; i < 6; ++i) {
-                final int dx2 = world.rand.nextInt(3) - 1;
-                final int dy2 = world.rand.nextInt(3) - 1;
-                final int dz2 = world.rand.nextInt(3) - 1;
-                final int px = dx2 + x;
-                final int py = dy2 + y;
-                final int pz = dz2 + z;
-                if (block.canPlaceBlockAt(world, px, py, pz) && world.isAirBlock(px, py, pz)) {
-                    world.setBlock(px, py, pz, block, meta, 3);
-                    world.playSoundEffect((double)x, (double)y, (double)z, "mob.endermen.portal", 1.0f, 1.0f);
-                    world.setBlockToAir(x, y, z);
-                    --stack.stackSize;
-                    return true;
-                }
+            world.playSound(null, pos, SoundEvents.ENTITY_ZOMBIE_VILLAGER_CONVERTED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            if (!player.capabilities.isCreativeMode) stack.shrink(1);
+            // TODO: Advancement
+            return EnumActionResult.SUCCESS;
+        }
+
+        // Regular behavior: Teleport targeted block to random adjacent position
+        for (int i = 0; i < 6; i++)
+        {
+            int dx = world.rand.nextInt(3) - 1;
+            int dy = world.rand.nextInt(3) - 1;
+            int dz = world.rand.nextInt(3) - 1;
+            BlockPos target = pos.add(dx, dy, dz);
+            if (world.isAirBlock(target) && block.canPlaceBlockAt(world, target))
+            {
+                world.setBlockState(target, state);
+                world.playSound(null, pos, SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                world.setBlockToAir(pos);
+                if (!player.capabilities.isCreativeMode) stack.shrink(1);
+                return EnumActionResult.SUCCESS;
             }
         }
-        return false;
-    }*/
+        return EnumActionResult.PASS;
+    }
 }
