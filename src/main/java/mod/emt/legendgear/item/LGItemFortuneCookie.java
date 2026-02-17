2f2342e3f6ec83afe4ac187be803e280e353e3da
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-02-17 12:05:31 +0100

    Refactor fortune cookie client-only behavior

--- src/main/java/mod/emt/legendgear/item/LGItemFortuneCookie.java
+++ src/main/java/mod/emt/legendgear/item/LGItemFortuneCookie.java
@@ -7,11 +7,12 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 import mod.emt.legendgear.init.LGSoundEvents;
@@ -32,28 +33,25 @@ public class LGItemFortuneCookie extends LGItemFood
         TooltipHelper.addWrappedTooltip(tooltip, TextFormatting.GRAY, I18n.format("tooltip.legendgear.fortune_cookie"));
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
     protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player)
     {
-        if (!world.isRemote)
+        // 1/3 chance to give a useful tip, otherwise give us a generic message
+        if (world.rand.nextInt(3) == 0)
         {
-            // 1/3 chance to give a useful tip, otherwise give us a generic message
-            if (world.rand.nextInt(3) == 0)
-            {
-                player.sendMessage(new TextComponentTranslation("fortunes.legendgear.hint." + world.rand.nextInt(countMessages("fortunes.legendgear.hint"))).setStyle(new Style().setItalic(true).setColor(TextFormatting.GOLD)));
-                player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 0.8F, 1.2F);
-                // TODO: Advancement
-            }
-            else
-            {
-                player.sendMessage(new TextComponentTranslation("fortunes.legendgear.generic." + world.rand.nextInt(countMessages("fortunes.legendgear.generic"))).setStyle(new Style().setItalic(true)));
-                player.world.playSound(null, player.posX, player.posY, player.posZ, LGSoundEvents.ITEM_FORTUNE_COOKIE_USE.getSoundEvent(), SoundCategory.PLAYERS, 2.0F, 0.8F + world.rand.nextFloat() * 0.4F);
-            }
+            player.sendMessage(new TextComponentTranslation("fortunes.legendgear.hint." + world.rand.nextInt(countMessages("fortunes.legendgear.hint"))).setStyle(new Style().setItalic(true).setColor(TextFormatting.GOLD)));
+            player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 0.8F, 1.2F);
         }
-
+        else
+        {
+            player.sendMessage(new TextComponentTranslation("fortunes.legendgear.generic." + world.rand.nextInt(countMessages("fortunes.legendgear.generic"))).setStyle(new Style().setItalic(true)));
+            player.playSound(LGSoundEvents.ITEM_FORTUNE_COOKIE_USE.getSoundEvent(), 2.0F, 0.8F + world.rand.nextFloat() * 0.4F);
+        }
         super.onFoodEaten(stack, world, player);
     }
 
+    @SideOnly(Side.CLIENT)
     private int countMessages(String translateKey)
     {
         int i;
